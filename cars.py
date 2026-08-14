0d3a1ca2b21523aebb6d7b5c9365d176d2ed2409
sylvesterleblac-code <sylvesterleblac@gmail.com>
2026-08-14 17:27:00 +0300

    printing models using functions and function calls

--- cars.py
+++
@@ -1,9 +0,0 @@
-cars = ['subaru', 'atenza', 'mercedes', 'lamborgini', 'subaru', 'ferrari', 'subaru']
-
-print(cars)
-
-while 'subaru' in cars:
-    cars.remove('subaru')
-
-print(f"We have now removed duplicate car here")
-print(cars)
--- rentals.py
+++
@@ -1,9 +0,0 @@
-import time
-
-print("------------------------")
-print("----JJ CAR RENTALS------")
-print("------------------------")
-
-car_to_be_rented = input("What car model woud you like to rent? ")
-time.sleep(2)
-print(f"Let me see if I can find you a {car_to_be_rented}")
